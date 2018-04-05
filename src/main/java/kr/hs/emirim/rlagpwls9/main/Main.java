1b12a62783ff5892fbd145612630b909fc4f0065
Hyejin Kim <hyepago@naver.com>
2018-04-05 10:06:02 +0900

    나령이에게 인사하는 루틴 작성

--- src/main/java/kr/hs/emirim/rlagpwls9/main/Main.java
+++ src/main/java/kr/hs/emirim/rlagpwls9/main/Main.java
@@ -7,8 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Greeter greeter = new Greeter();
-		greeter.setFormat("%s, 안녕하세요!");
-		String msg = greeter.greet("스프링"); //msg는 "스프링, 안녕하세요!"가 된다.
+		greeter.setFormat("%s씨, 안녕하세요!");
+		String msg = greeter.greet("김나령"); //msg는 "스프링, 안녕하세요!"가 된다.
 		System.out.println(msg);
 	}
 
